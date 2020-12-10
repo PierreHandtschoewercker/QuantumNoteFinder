054b7687963d919862da005229a6c2c6cb5629ac
Pierre Handtschoewercker <pierre.handtschoewercker@gmail.com>
2020-12-10 22:41:05 +0100

    projet rendu tel-quel

--- Grover.py
+++ Grover.py
@@ -1,70 +1,31 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-# importing Qiskit
-from qiskit import IBMQ, Aer, QuantumCircuit, ClassicalRegister, QuantumRegister, execute
-from qiskit.providers.ibmq import least_busy
-from qiskit.quantum_info import Statevector
-
-# import basic plot tools
-from qiskit.visualization import plot_histogram
-
-freq = 440
-notes = {'A': 110, 'B': 123.5, 'C': 65.41, 'D': 73.42, 'E': 82.41, 'F': 87.31, 'G': 98}
-#        000       001          010         011         100         101         111
+from qiskit import *
+from qiskit.aqua.algorithms import Grover
+from qiskit.aqua.components.oracles import TruthTableOracle
+from qiskit.aqua import QuantumInstance
+import math
 
 
-def findNoteFororacle(qc, freq, notes):
-    for index,n in enumerate(notes):
-        if (notes[n] - 2) < freq < (notes[n] + 2):
-            qc.cz()
+#          0000        0001        0010        0011       0100       0101       0110       1000      1001       1010      1011         1100
+
+def buildTruthTable(notes, freq):
+    truthTable = ''
+    for n in notes:
+        if float(notes[n] - 3) < float(freq) < float(notes[n] + 3):
+            truthTable += '1'
+        else:
+            truthTable += '0'
+    return truthTable
 
 
-
-n = 4
-oracle_circuit = QuantumCircuit(n, name='Oracle')
-oracle_circuit.cz(0, 3)  # Oracle
-oracle_circuit.cz(1, 3)  # Oracle
-oracle_circuit.cz(2, 3)  # Oracle
-oracle_ex4 = oracle_circuit.to_gate()
-oracle_circuit.draw()
-
-
-# superposition of all qbits
-def initialize_s(qc, qubits):
-    """Apply a H-gate to 'qubits' in qc"""
-    for q in qubits:
-        qc.h(q)
-    return qc
-
-
-def diffuser(nqubits):
-    qc = QuantumCircuit(nqubits)
-    # Apply transformation |s> -> |00..0> (H-gates)
-    for qubit in range(nqubits):
-        qc.h(qubit)
-    # Apply transformation |00..0> -> |11..1> (X-gates)
-    for qubit in range(nqubits):
-        qc.x(qubit)
-    # Do multi-controlled-Z gate
-    qc.h(nqubits - 1)
-    qc.mct(list(range(nqubits - 1)), nqubits - 1)  # multi-controlled-toffoli
-    qc.h(nqubits - 1)
-    # Apply transformation |11..1> -> |00..0>
-    for qubit in range(nqubits):
-        qc.x(qubit)
-    # Apply transformation |00..0> -> |s>
-    for qubit in range(nqubits):
-        qc.h(qubit)
-    # We will return the diffuser as a gate
-    U_s = qc.to_gate()
-    U_s.name = "$U_superposition$"
-    return U_s
-
-
-grover_circuit = QuantumCircuit(n)
-grover_circuit = initialize_s(grover_circuit, [0, 1, 2, 3])
-grover_circuit.append(oracle_ex4, [0, 1, 2, 3])
-grover_circuit.append(diffuser(n), [0, 1, 2, 3])
-grover_circuit.measure_all()
-grover_circuit.draw()
+def grover(freq):
+    notes = {'C': 65.41, 'C#': 69.30, 'D': 73.42, 'D#': 77.78, 'E': 82.41, 'F': 87.31, 'F#': 92.50, 'G': 98,
+             'G#': 103.83,
+             'A': 110, 'A#': 116.54, 'B': 123.5, '0': 0, '1': 0, '2': 0, '3': 0
+             }
+    truthtable = buildTruthTable(notes, freq)
+    oracle = TruthTableOracle(truthtable)
+    grover = Grover(oracle)
+    result = grover.run(QuantumInstance(BasicAer.get_backend('qasm_simulator'), shots=1024))
+    indice_note = int(result['top_measurement'], 2)
+    key = list(notes)[indice_note]
+    print(freq,key)
--- QuantumNoteFinder.py
+++ QuantumNoteFinder.py
@@ -2,35 +2,17 @@ try:
     import pyaudio
     import numpy as np
     import pylab
-    import matplotlib.pyplot as plt
     from scipy.io import wavfile
     import time
     import sys
     import seaborn as sns
     import aubio
+    import Grover
+    import math
 except:
     print("Something didn't import")
 
-"""i=0
-f,ax = plt.subplots(2)
-
-# Prepare the Plotting Environment with random starting values
-x = np.arange(10000)
-y = np.random.randn(10000)
-
-# Plot 0 is for raw audio data
-li, = ax[0].plot(x, y)
-ax[0].set_xlim(0,1000)
-ax[0].set_ylim(-5000,5000)
-ax[0].set_title("Raw Audio Signal")
-# Plot 1 is for the FFT of the audio
-li2, = ax[1].plot(x, y)
-ax[1].set_xlim(0,5000)
-ax[1].set_ylim(-100,100)
-ax[1].set_title("Fast Fourier Transform")
-# Show the plot, but without blocking updates
-plt.pause(0.01)
-plt.tight_layout()"""
+is_exec = 0
 win_s = 4096
 FORMAT = pyaudio.paFloat32  # We use 16bit format per sample
 CHANNELS = 1
@@ -45,11 +27,7 @@ def callback(in_data, frame_count, time_info, status):
     # and make sure it's not imaginary
     dfft = 10. * np.log10(abs(np.fft.rfft(audio_data)))
 
-    # Force the new data into the plot, but without redrawing axes.
-    # If uses plt.draw(), axes are re-drawn every time
-    # print audio_data[0:10]
-    # print dfft[0:10]
-    # print
+
     samples = np.fromstring(in_data,
                             dtype=aubio.float_type)
     # Finally get the pitch.
@@ -62,7 +40,10 @@ def callback(in_data, frame_count, time_info, status):
     volume = "{:6f}".format(volume)
 
     # Finally print the pitch and the volume.
-    print(str(pitch) + " " + str(volume))
+    #print(str(pitch) + " " + str(volume))
+    if math.floor(pitch) != 0:
+        Grover.grover(pitch)
+
     return in_data, pyaudio.paContinue
 
 
